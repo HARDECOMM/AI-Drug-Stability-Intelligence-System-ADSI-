0b2dc591a735fe4b3eb275be735081d22689096b
Ademoye Haruna Adegoke <Hardecomm>
2026-04-18 19:57:48 +0100

    add backend url to simulator

--- backend/simulator.py
+++ backend/simulator.py
@@ -2,7 +2,7 @@ import requests
 import time
 import random
 
-URL = "http://127.0.0.1:5000/predict"
+URL = "https://ai-drug-stability-intelligence-system.onrender.com/predict"  # "http://127.0.0.1:5000/predict"
 
 DRUGS = ["Insulin", "Amoxicillin", "Paracetamol", "Aspirin", "Metformin"]
 LOCATIONS = [
